b4e9c7048c0efc12a744acda50ec0738b4064244
Lans12345 <olanalans12345@gmail.com>
2024-11-22 15:04:28 +0800

    added nurse dialog ui

--- lib/screens/nurse_home_screen.dart
+++ lib/screens/nurse_home_screen.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:wecare/screens/calendar_screen.dart';
 import 'package:wecare/widgets/text_widget.dart';
 
-class NurseHomeScreen extends StatelessWidget {
+class NurseHomeScreen extends StatefulWidget {
   const NurseHomeScreen({super.key});
 
+  @override
+  State<NurseHomeScreen> createState() => _NurseHomeScreenState();
+}
+
+class _NurseHomeScreenState extends State<NurseHomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +19,7 @@ class NurseHomeScreen extends StatelessWidget {
         child: FloatingActionButton(
           child: const Icon(Icons.timer_outlined),
           onPressed: () {
+            _showTimeSelectorDialog();
             // Navigator.of(context).push(MaterialPageRoute(
             //     builder: (context) => const CalendarScreen()));
           },
@@ -121,6 +127,84 @@ class NurseHomeScreen extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+
+  TimeOfDay morningTime = const TimeOfDay(hour: 7, minute: 0);
+  TimeOfDay noonTime = const TimeOfDay(hour: 12, minute: 0);
+  TimeOfDay eveningTime = const TimeOfDay(hour: 18, minute: 0);
+
+  Future<void> _selectTime(BuildContext context, TimeOfDay initialTime,
+      Function(TimeOfDay) onTimeSelected) async {
+    final TimeOfDay? pickedTime = await showTimePicker(
+      context: context,
+      initialTime: initialTime,
+    );
+    if (pickedTime != null) {
+      onTimeSelected(pickedTime);
+    }
+  }
+
+  void _showTimeSelectorDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Set Time'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              _buildTimePickerRow(
+                context,
+                label: 'MORNING',
+                selectedTime: morningTime,
+                onTimeChanged: (time) => setState(() => morningTime = time),
+              ),
+              const SizedBox(height: 16),
+              _buildTimePickerRow(
+                context,
+                label: 'NOON',
+                selectedTime: noonTime,
+                onTimeChanged: (time) => setState(() => noonTime = time),
+              ),
+              const SizedBox(height: 16),
+              _buildTimePickerRow(
+                context,
+                label: 'EVENING',
+                selectedTime: eveningTime,
+                onTimeChanged: (time) => setState(() => eveningTime = time),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildTimePickerRow(
+    BuildContext context, {
+    required String label,
+    required TimeOfDay selectedTime,
+    required Function(TimeOfDay) onTimeChanged,
+  }) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(label, style: const TextStyle(fontWeight: FontWeight.bold)),
+        TextButton(
+          onPressed: () => _selectTime(context, selectedTime, onTimeChanged),
+          child: Text(
+            selectedTime.format(context),
+            style: const TextStyle(color: Colors.blue),
+          ),
+        ),
+      ],
     );
   }
 }
