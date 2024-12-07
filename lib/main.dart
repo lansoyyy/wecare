44d6c5a81fb0e6a7a6ba1ede367d9efe8c57c41c
Lans12345 <olanalans12345@gmail.com>
2024-12-07 08:42:00 +0800

    initialy added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:wecare/screens/home_screen.dart';
 import 'package:wecare/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(
+//     name: 'esti-32772',
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+//   runApp(const MyApp());
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
